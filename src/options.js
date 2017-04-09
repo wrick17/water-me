8050ce5639576033ef2a21d2fe3c160a6d925f80
Pratyush Poddar <wrick17@gmail.com>
2017-04-09 22:18:15 +0530

    code formatting and added gitignore

--- src/options.js
+++ src/options.js
@@ -1,48 +1,48 @@
 window.onload = function(){
 
-var defaultTriggerInterval = 1;
-var defaultBlockDuration = 1;
+	var defaultTriggerInterval = 1;
+	var defaultBlockDuration = 1;
 
-function loadOptions() {
-	 chrome.storage.local.get("settings", function(showObj){
+	function loadOptions() {
+		 chrome.storage.local.get("settings", function(showObj){
 
-		 console.log(showObj);
+			 console.log(showObj);
 
-    var triggerInterval = showObj.settings.triggerInterval;
-    if (!triggerInterval) {
-      triggerInterval = defaultTriggerInterval;
-    }
+	    var triggerInterval = showObj.settings.triggerInterval;
+	    if (!triggerInterval) {
+	      triggerInterval = defaultTriggerInterval;
+	    }
 
-    var blockDuration = showObj.settings.blockDuration;
-    if (!blockDuration) {
-      blockDuration = defaultBlockDuration;
-    }
+	    var blockDuration = showObj.settings.blockDuration;
+	    if (!blockDuration) {
+	      blockDuration = defaultBlockDuration;
+	    }
 
-		document.getElementById("triggerInterval").value = triggerInterval;
+			document.getElementById("triggerInterval").value = triggerInterval;
 
-		document.getElementById("blockDuration").value = blockDuration;
+			document.getElementById("blockDuration").value = blockDuration;
 
-  })
-}
-loadOptions();
-document.getElementById("saveButton").addEventListener("click",function() {
+	  })
+	}
+	loadOptions();
+	document.getElementById("saveButton").addEventListener("click",function() {
 
-  var extensionStorageShow ={};
+	  var extensionStorageShow ={};
 
-	var select1 = document.getElementById("triggerInterval");
-	console.log(select1.value);
-	extensionStorageShow.triggerInterval = select1.value;
+		var select1 = document.getElementById("triggerInterval");
+		console.log(select1.value);
+		extensionStorageShow.triggerInterval = select1.value;
 
-  var select2 = document.getElementById("blockDuration");
-  extensionStorageShow.blockDuration = select2.value;
+	  var select2 = document.getElementById("blockDuration");
+	  extensionStorageShow.blockDuration = select2.value;
 
 
-	var extensionStorage = {
-    settings:extensionStorageShow
-  }
-  chrome.storage.local.set(extensionStorage);
-  alert("Your changes are saved");
+		var extensionStorage = {
+	    settings:extensionStorageShow
+	  }
+	  chrome.storage.local.set(extensionStorage);
+	  alert("Your changes are saved");
 
-});
+	});
 
 };
